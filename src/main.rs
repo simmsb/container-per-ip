d9e91fa9e43f70a4c64af009be0ea16b8dd150b3
Ben Simms <ben@bensimms.moe>
2019-11-22 02:11:20 +0000

    Some initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+use snafu::Snafu;
+use structopt::StructOpt;
+
+use tokio::prelude::*;
+
+#[derive(Debug, Snafu)]
+enum Error {}
+
+type Result<T, E = Error> = std::result::Result<T, E>;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "container-per-ip", about = "Run a container per client ip")]
+struct Opt {
+    #[structopt()]
+    /// The docker image to run for each ip
+    image: String,
+
+    #[structopt(long)]
+    /// Should the containers be started with the `--privileged` flag
+    privileged: bool,
+
+    #[structopt(short, long)]
+    /// Ports to listen on (tcp only currently)
+    ports: Vec<u64>,
+
+    #[structopt(long)]
+    /// Timeout (seconds) after an IPs last connection disconnects before
+    /// killing the associated container
+    timeout: Option<f64>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
+
+    Ok(())
 }
