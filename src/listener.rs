9c72525c84092f26816a4cc31d8e625c4d39b9ba
Ben Simms <ben@bensimms.moe>
2020-01-28 03:16:58 +0000

    Use tokio::select! instead of futures::select!

--- src/listener.rs
+++ src/listener.rs
@@ -1,14 +1,11 @@
 use std::net::Ipv4Addr;
-use tokio::task::JoinHandle;
 
+use tokio::select;
+use tokio::task::JoinHandle;
 use tokio::net::TcpListener;
 use tokio::sync::{mpsc, oneshot};
 
 use log::{error, info};
-
-use futures::future::FutureExt;
-use futures::pin_mut;
-use futures::select;
 
 use snafu::{ResultExt, Snafu};
 
@@ -25,7 +22,7 @@ type Result<T, E = Error> = std::result::Result<T, E>;
 pub async fn listen_on(
     port: u16,
     events: mpsc::UnboundedSender<ConnectionEvent>,
-    stop: oneshot::Receiver<()>,
+    mut stop: oneshot::Receiver<()>,
 ) -> Result<JoinHandle<()>> {
     let mut listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, port))
         .await
@@ -34,18 +31,13 @@ pub async fn listen_on(
     Ok(tokio::spawn(async move {
         info!("Starting listener on port {}", port);
 
-        let mut stop = FutureExt::fuse(stop);
-
         loop {
-            let listener_accept = listener.accept().fuse();
-            pin_mut!(listener_accept);
-
             select! {
-                _ = stop => {
+                _ = &mut stop => {
                     info!("Stopping listener on port {}", port);
                     return;
                 },
-                r = listener_accept => {
+                r = listener.accept() => {
                     match r {
                         Ok((socket, addr)) => {
                             info!("Incoming connection from {} on port {}", addr, port);
